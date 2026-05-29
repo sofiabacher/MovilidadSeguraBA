7bcf1db1f304119fbb68a12e3d4645d1a3eb223b
Sofia Bacher <bacher.sofiaet36@gmail.com>
2026-05-29 00:44:25 -0300

    Update analisis.py

--- services/analisis.py
+++ services/analisis.py
@@ -1,47 +1,5 @@
 import pandas as pd
 
-def crear_dataframe_transito():
-    datos = {
-        "hora": [
-            "08:00",
-            "09:00",
-            "10:00",
-            "11:00",
-            "12:00",
-            "13:00",
-            "14:00"
-        ],
-         "flujo": [
-            3200,
-            5400,
-            6100,
-            4500,
-            3900,
-            7100,
-            6800
-        ],
-        "velocidad": [
-            52,
-            40,
-            28,
-            35,
-            45,
-            22,
-            25
-        ],
-        "zona": [
-            "Centro",
-            "Centro",
-            "Norte",
-            "Sur",
-            "Oeste",
-            "Centro",
-            "Norte"
-        ]
-    }
-
-    return pd.DataFrame(datos)
-
 def calcular_metricas(df):
     flujo_total = df["flujo"].sum()
     velocidad_promedio = round(df["velocidad"].mean(), 2)
