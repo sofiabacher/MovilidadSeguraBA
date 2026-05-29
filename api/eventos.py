4491da55bd4ba4a598666842ddbe5e972372d444
sofiabacher <bacher.sofiaet36@gmail.com>
2026-05-29 00:43:41 -0300

    create repo movilidadseguraBA

--- api/eventos.py
+++ api/eventos.py
@@ -1,20 +1,23 @@
 import requests
 import pandas as pd
+import os
 
-from api.auth import obtener_token
+from dotenv import load_dotenv
+load_dotenv()
+
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
 EVENTOS_URL = "https://api-transporte.buenosaires.gob.ar/eventos"
 
 def obtener_eventos():
-    token = obtener_token()
-
-    if not token:
-        return pd.DataFrame()
-    
-    headers = {"Authorization": f"Bearer {token}"}
+    params = {
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET
+    }
 
     try:
-        response = requests.get(EVENTOS_URL, headers=headers)
+        response = requests.get(EVENTOS_URL, params=params)
         
         if response.status_code == 200:
             data = response.json()
--- api/transito.py
+++ api/transito.py
@@ -1,25 +1,31 @@
 import requests
 import pandas as pd
+import os
 
-from api.auth import obtener_token
+from dotenv import load_dotenv
+load_dotenv()
+
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
 TRANSITO_URL = "https://api-transporte.buenosaires.gob.ar/transito"
 
 def obtener_datos_transito():
-    token = obtener_token()
-
-    if not token:
-        return pd.DataFrame()
-    
-    headers = {"Authorization": f"Bearer {token}"}
+    params = {
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET
+    }
 
     try:
-        response = requests.get(TRANSITO_URL, headers=headers)
+        response = requests.get(TRANSITO_URL, params=params)
+        print(response.status_code)
         
         if response.status_code == 200:
+            print("REPRESENTACIÓN DEL TEXTO RECIBIDO:")
+            print(response.text[:500])
+            
             data = response.json()
-            df = pd.DataFrame(data)
-            return df
+            return pd.DataFrame(data)
 
         else:
             print(f"Error obteniendo datos: {response.text}")
